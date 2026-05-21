78c8678aeb55983eed32870a7feee728733480cf
catlisa332 <catlisa332@users.noreply.github.com>
2026-05-21 05:44:01 +0000

    deploy: a016f3d620b35696c57036a7a7d727175d72c525

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4023933960" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1663820599" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
